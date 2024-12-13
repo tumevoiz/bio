b05a1cf23e10df7daa45a676ce10ef456488ec9f
Jakub Al-Khalili <jakubalkhalili@gmail.com>
2024-12-13 21:49:28 +0100

    Add authentication filter

--- src/main/kotlin/bio/BioApplication.kt
+++ src/main/kotlin/bio/BioApplication.kt
@@ -1,33 +1,25 @@
 package bio
 
+import bio.auth.AuthenticationFilter
 import bio.auth.AuthenticationRoutes
 import bio.auth.BearerTokenAuthenticationService
 import bio.data.*
 import bio.prelude.Argon2HashingAlgorithm
-import bio.users.UnsafeUserRepository
-import bio.users.UserRepository
-import bio.users.UserRoutes
-import bio.users.UserService
-import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Basic
+import bio.users.*
+import org.http4k.contract.ContractRoute
 import org.http4k.contract.bind
 import org.http4k.contract.contract
 import org.http4k.contract.meta
 import org.http4k.contract.openapi.ApiInfo
 import org.http4k.contract.openapi.v3.ApiServer
 import org.http4k.contract.openapi.v3.OpenApi3
-import org.http4k.contract.security.BasicAuthSecurity
+import org.http4k.contract.security.BearerAuthSecurity
 import org.http4k.contract.ui.swaggerUiLite
 import org.http4k.core.*
-import org.http4k.core.Method.*
 import org.http4k.core.Status.Companion.OK
 import org.http4k.filter.CorsPolicy
-import org.http4k.filter.DebuggingFilters.PrintRequest
-import org.http4k.filter.OpenTelemetryMetrics
-import org.http4k.filter.OpenTelemetryTracing
 import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson
-import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
@@ -47,6 +39,7 @@ fun main() {
     val authenticationService = BearerTokenAuthenticationService(cachingProvider, userRepository, hashingAlgorithm)
 
     val authenticationRoutes = AuthenticationRoutes(authenticationService)
+    val authFilter = AuthenticationFilter(authenticationService)
 
     val userService = UserService(hashingAlgorithm, userRepository)
     val userRoutes = UserRoutes(userService)
@@ -60,6 +53,7 @@ fun main() {
         descriptionPath = "/docs/openapi.json"
         routes += authenticationRoutes.login()
         routes += userRoutes.createUser()
+        routes += healthRoute(authFilter)
     }
 
     val api = routes(
@@ -72,4 +66,14 @@ fun main() {
     val server = ServerFilters.Cors(CorsPolicy.UnsafeGlobalPermissive).then(api).asServer(Undertow(HTTP_SERVER_PORT)).start()
 
     println("Server started on " + server.port())
+}
+
+fun healthRoute(authFilter: AuthenticationFilter): ContractRoute {
+    return "/health_check" meta {
+        summary = "Health Check"
+        description = "Health check for application (secured)"
+        security = BearerAuthSecurity(authFilter)
+    } bindContract Method.GET to { req: Request ->
+        Response(OK).body("User UUID is ${req.header("x-user-uuid")}")
+    }
 }
